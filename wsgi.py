aa62322683552de16756abdfc61af4d5322052ec
netcomlimitedsystems <netcomlimitedsystems@gmail.com>
2025-10-02 00:57:48 +0300

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -2,17 +2,17 @@ import os
 import sys
 
 # Path to your project
-path = '/home/saidi/Desktop/netcom_learning'
+path = '/home/netcomlimitedsystems/netcomlimitedsystems'
 if path not in sys.path:
     sys.path.append(path)
 
-# Virtualenv
-venv_path = '/home/saidi/Desktop/netcom_learning/.env'
-
+# Activate virtual environment
+venv_path = '/home/netcomlimitedsystems/netcomlimitedsystems/venv'
 activate_this = os.path.join(venv_path, 'bin/activate_this.py')
 with open(activate_this) as f:
     exec(f.read(), dict(__file__=activate_this))
 
+# Django settings
 os.environ['DJANGO_SETTINGS_MODULE'] = 'netcom_learning.settings'
 
 from django.core.wsgi import get_wsgi_application
